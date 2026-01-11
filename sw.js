bba6d2cc734d6e904dc463384643d32141f38f9f
Christian <christianvn15@yahoo.com>
2026-01-11 15:08:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,23 @@
-const CACHE = 'creator-cache-v1';
+const CACHE = 'creator-cache-v2';
 const ASSETS = [
   './',
   './index.html',
   './styles.css',
   './app.js',
-  './db.js',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  // External libs to cache for offline 3D
-  'https://unpkg.com/three@0.160.0/build/three.min.js',
-  'https://unpkg.com/three@0.160.0/examples/js/controls/OrbitControls.js'
+  './storage.js',
+  './manifest.webmanifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null))))
   );
   self.clients.claim();
 });
@@ -36,7 +29,6 @@ self.addEventListener('fetch', (event) => {
       const fetchPromise = fetch(request).then(resp => {
         const copy = resp.clone();
         caches.open(CACHE).then(cache => {
-          // Only cache GET and same-origin or whitelisted CDN
           if (request.method === 'GET' &&
               (request.url.startsWith(self.location.origin) ||
                request.url.startsWith('https://unpkg.com/'))) {
